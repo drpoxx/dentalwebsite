d0235d942cb712695f4d5fbe9a64c328c7f54de0
pochs <ochs.pascal@gmail.com>
2021-10-15 17:12:16 +0100

    make the contact form live

--- dentalwebsite/views.py
+++ dentalwebsite/views.py
@@ -5,4 +5,17 @@ def home(request):
     return render(request, 'home.html', {})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        return render(request, 'contact.html', {
+            'message_name': message_name,
+            'message_email': message_email,
+            'message': message
+        })
+
+    else:
+        # return the page:
+        return render(request, 'contact.html', {})
